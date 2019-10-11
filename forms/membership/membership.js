228afbf1958f309b93fff13a0cf219cfa148d7a6
Daniel Kellmeyer <dsk96139@creighton.edu>
2019-10-11 17:23:40 -0500

    Initial commit

---
+++ forms/membership/membership.js
@@ -0,0 +1,15 @@
+/*
+var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+btnCheck.onclick=function(){
+  fName = inptFirstName.value
+  if (members.includes(fName)){
+    members.push(fName)
+    NSB.MsgBox("You are a member!")
+    }
+  else {
+    NSB.MsgBox("You are not a member")
+    inptFirstName.value = ""
+    }
+}
+*/
